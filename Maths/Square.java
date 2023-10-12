d2366b408cdfde77cc83c93fb7d80e70fc8b00c3
PrashanthiV1234 <prashanthivelpula824@gmail.com>
2023-10-12 15:27:53 +0530

    Square

---
+++ Maths/Square.java
@@ -0,0 +1,31 @@
+public class Square {
+    public static void main(String[] args) {
+        System.out.println("\n\t square \n");
+        int n=4;
+        for(int i=1;i<=n;i++){
+        // Ls(n);
+        for(int j=1;j<=n;j++){
+            System.out.print("\t*"); 
+    }   
+        System.out.println("\n");
+        }
+        
+
+    }
+    private static void Ls(int n){
+       for(int i=1;i<=n;i++){
+            System.out.print("\t*"); 
+    }
+}
+    
+}
+
+            square 
+
+        *       *       *       *
+
+        *       *       *       *
+
+        *       *       *       *
+
+        *       *       *       *
